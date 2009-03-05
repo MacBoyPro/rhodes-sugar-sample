6cf5de5becd5f864959ed05a9dd98e093b279ae0
lars <larsburgess@gmail.com>
2009-03-05 10:46:02 -0800

    fixing error messages

--- app/Settings/controller.rb
+++ app/Settings/controller.rb
@@ -4,22 +4,21 @@ require 'rho/rhocontroller'
 class SettingsController < Rho::RhoController
   
   def index
-    puts 'inside index'
     @msg = @params['msg']
     render
   end
 
   def login
+    @msg = @params['msg']
     render :action => :login
   end
 
   def do_login
-    puts "PARAMS: #{@params.inspect}"
     if @params['login'] and @params['password']
       success = SyncEngine::login(@params['login'], @params['password'])
     else
       @msg = "You entered an invalid login/password, please try again."
-      render :action => :login
+      render :action => :login, :query => {:msg => @msg}
     end
     if success > 0
       # run sync if we were successful
@@ -27,14 +26,14 @@ class SettingsController < Rho::RhoController
       redirect Rho::RhoConfig.start_path
     else
       @msg = "You entered an invalid login/password, please try again."
-      render :action => :login
+      render :action => :login, :query => {:msg => @msg}
     end
   end
   
   def logout
     SyncEngine::logout
     @msg = "You have been logged out."
-    render :action => :login
+    render :action => :login, :query => {:msg => @msg}
   end
   
   def reset
@@ -43,12 +42,12 @@ class SettingsController < Rho::RhoController
   
   def do_reset
     SyncEngine::trigger_sync_db_reset
-    @msg = "Database will be deleted on next sync."
-    redirect Rho::RhoConfig.start_path
+    SyncEngine::dosync
+    @msg = "Database has been reset."
+    redirect :action => :index, :query => {:msg => @msg}
   end
   
   def do_sync
-    puts 'inside do_sync'
     SyncEngine::dosync
     @msg =  "Sync has been triggered."
     redirect :action => :index, :query => {:msg => @msg}
