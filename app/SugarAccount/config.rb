f049526f061362559f8d68dd2fcb9ad7e5872fb4
lars <larsburgess@gmail.com>
2009-03-22 14:30:41 -0700

    changing default url to dev.rhosync.rhohub.com

--- app/SugarAccount/config.rb
+++ app/SugarAccount/config.rb
@@ -1,3 +1,3 @@
 require 'rho'
 
-Rho::RhoConfig::add_source("SugarAccount", {"url"=>"http://rhosyncdev.rhohub.com/apps/1/sources/1", "source_id"=>1})
+Rho::RhoConfig::add_source("SugarAccount", {"url"=>"http://dev.rhosync.rhohub.com/apps/1/sources/1", "source_id"=>1})
