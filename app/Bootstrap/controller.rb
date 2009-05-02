5284c566b5f4838d490927c1ed1cc5414609b017
lars <larsburgess@gmail.com>
2009-05-01 18:11:06 -0700

    adding offline demo to rhodes-sugar-sample, still need to cleanup UI

---
+++ app/Bootstrap/controller.rb
@@ -0,0 +1,32 @@
+require 'rho'
+require 'rho/rhocontroller'
+
+class BootstrapController < Rho::RhoController
+
+  #GET /Bootstrap
+  def index
+    render
+  end
+  
+  def load_offline_data
+    first_row=true
+    columns = []
+    ['sources','object_values'].each do |filename|
+      File.open(File.join(Rho::RhoFSConnector.get_base_app_path,'app','fixtures',filename+'.txt')).each do |line|
+        if first_row
+          columns = line.chomp.split('|'); first_row = false; next;
+        end
+        parts = line.chomp.split('|')
+        
+        row = {}
+        columns.each_with_index do |col,idx| 
+          row[col] = parts[idx]
+        end
+        Rhom::RhomDbAdapter.insert_into_table(filename,row)
+      end
+      columns = []
+      first_row = true
+    end
+    redirect Rho::RhoConfig.start_path
+  end
+end
