155ebd5a37465873a5cc5a60cd4142946028054e
lars <larsburgess@gmail.com>
2009-05-12 14:36:42 -0700

    use load_offline_data from rhoutils

--- app/Bootstrap/controller.rb
+++ app/Bootstrap/controller.rb
@@ -1,5 +1,6 @@
 require 'rho'
 require 'rho/rhocontroller'
+require 'rho/rhoutils'
 
 class BootstrapController < Rho::RhoController
 
@@ -9,25 +10,7 @@ class BootstrapController < Rho::RhoController
   end
   
   def load_offline_data
-    first_row=true
-    columns = []
-    ['sources','object_values'].each do |filename|
-      Rhom::RhomDbAdapter.delete_all_from_table(filename)
-      File.open(File.join(Rho::RhoFSConnector.get_base_app_path,'app','fixtures',filename+'.txt')).each do |line|
-        if first_row
-          columns = line.chomp.split('|'); first_row = false; next;
-        end
-        parts = line.chomp.split('|')
-        
-        row = {}
-        columns.each_with_index do |col,idx| 
-          row[col] = parts[idx]
-        end
-        Rhom::RhomDbAdapter.insert_into_table(filename,row)
-      end
-      columns = []
-      first_row = true
-    end
+    Rho::RhoUtils.load_offline_data(['object_values','sources'])
     redirect Rho::RhoConfig.start_path
   end
 end
